cb2f05a2b85e34c29cda93d41f6693d191f0b067
dceoy <dnarsil+github@gmail.com>
2022-08-06 05:53:40 +0900

    Improve the interface

--- kalmanfilter.py
+++ kalmanfilter.py
@@ -16,9 +16,8 @@ class KalmanFilter(object):
         self.r = r                          # measurement variance
         self.y = np.array([np.nan])         # observation
         self.__keep_history = keep_history
-        self.__logger.debug('vars(self): {}'.format(vars(self)))
 
-    def fit(self, y, x0=None, v0=None, q=None, r=None):
+    def filter(self, y, x0=None, v0=None, q=None, r=None):
         x0_ = x0 or self.x[-1]
         v0_ = v0 or self.v[-1]
         q_ = q or self.q
@@ -45,32 +44,45 @@ class KalmanFilter(object):
         )
 
 
-class KalmanFilterOptimizer(object):
-    def __init__(self, y, x0=0.0, v0=1.0, q0=1.0, r0=1.0, method='L-BFGS-B'):
+class OptimizedKalmanFilter(object):
+    def __init__(self, y, x0=0.0, v0=1.0, q0=1.0, r0=1.0, method='L-BFGS-B',
+                 keep_history=False):
         self.__logger = logging.getLogger(__name__)
+        self.y = y                      # observation
         self.x0 = x0                    # estimate of x
         self.v0 = v0                    # error estimate
         self.q0 = q0                    # process variance
         self.r0 = r0                    # measurement variance
         self.__method = method          # method for scipy.optimize.minimize()
+        self.__keep_history = keep_history
+        self.q = None
+        self.r = None
+        self.kf = None
+        self.optimize_parameters()
         self.__logger.debug('vars(self): {}'.format(vars(self)))
 
-    def optimize(self, y):
+    def optimize_parameters(self):
         res = minimize(
             fun=self._loss, x0=np.array([self.q0, self.r0]),
-            args=(y, self.x0, self.v0), method=self.__method
+            args=(self.y, self.x0, self.v0), method=self.__method
         )
         self.__logger.info(f'{os.linesep}{res}')
-        q = res.x[0]
-        self.__logger.info(f'process variance: {q}')
-        r = res.x[1]
-        self.__logger.info(f'measurement variance: {r}')
-        return q, r
+        self.q = res.x[0]
+        self.__logger.info(f'process variance: {self.q}')
+        self.r = res.x[1]
+        self.__logger.info(f'measurement variance: {self.r}')
+        self.kf = KalmanFilter(
+            x0=self.x0, v0=self.v0, q=self.q, r=self.r,
+            keep_history=self.__keep_history
+        )
 
     @staticmethod
     def _loss(x, *args):
         return KalmanFilter(
             x0=args[1], v0=args[2], q=x[0], r=x[1], keep_history=True
-        ).fit(y=args[0]).pipe(
+        ).filter(y=args[0]).pipe(
             lambda d: np.sum(np.square(d['y'] - d['x']))
         )
+
+    def filter(self, y=None, **kwargs):
+        return self.kf.filter(y=(y or self.y), **kwargs)
