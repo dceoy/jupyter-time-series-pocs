30111b5ab5a5538a4247f3b1294c6e361281a04f
dceoy <dnarsil+github@gmail.com>
2022-08-10 01:10:46 +0900

    Use scipy.optimize.brute

--- ema/ema.py
+++ ema/ema.py
@@ -5,7 +5,7 @@ import os
 from pprint import pformat
 
 import numpy as np
-import pandas as pd
+from scipy.optimize import brute
 from scipy.stats import norm
 
 
@@ -15,43 +15,47 @@ class OptimizedEma(object):
         self.__logger = logging.getLogger(__name__)
         self.y = y      # observation
         self.test_size = (test_size or int(y.size / 2))
-        assert self.test_size > 0, f'invalid test size: {self.test_size}'
-        self.__logger.info(f'self.test_size: {test_size}')
+        assert self.test_size > 0, f'invalid test size: {test_size}'
+        self.__logger.info(f'self.test_size: {self.test_size}')
         assert y.size > self.test_size, f'too short y: {y.size}'
-        self.ewm_span_range = (span_range or (2, int(y.size / 2)))
+        self.ewm_span_range = slice(*(span_range or (2, int(y.size / 2))), 1)
         self.__logger.info(f'self.ewm_span_range: {self.ewm_span_range}')
-        self.ewm_add_kwargs = {
-            'adjust': adjust, 'ignore_na': ignore_na, **kwargs
-        }
+        self.ewm_add_kwargs = {'adjust': adjust, 'ignore_na': ignore_na}
+        self.scipy_optimize_brute_add_kwargs = kwargs
         self.ewm_span = None
         self.__logger.debug('vars(self):' + os.linesep + pformat(vars(self)))
 
     def optimize_ewm_span(self):
         np.seterr(all='raise')
-        loss = pd.Series({
-            i: self._loss(
-                span=i, y=self.y, test_size=self.test_size,
-                **self.ewm_add_kwargs
-            ) for i in range(*self.ewm_span_range)
-        })
-        self.__logger.info(f'loss: {loss}')
-        self.ewm_span = loss.idxmin()
+        result = brute(
+            func=self._loss, ranges=(self.ewm_span_range,),
+            args=(self.y, self.test_size, self.ewm_add_kwargs),
+            finish=None, **self.scipy_optimize_brute_add_kwargs
+        )
+        self.__logger.info(f'result: {result}')
+        self.ewm_span = int(result)
         self.__logger.info(f'self.ewm_span: {self.ewm_span}')
 
     @staticmethod
-    def _loss(span, y, test_size, **kwargs):
-        ewm = y.ewm(span=span, **kwargs)
-        return (
-            -2 * np.sum(
-                np.log(
-                    norm.pdf(
-                        x=y.tail(test_size),
-                        loc=ewm.mean().shift().tail(test_size),
-                        scale=ewm.std(ddof=1).shift().tail(test_size)
+    def _loss(x, *args):
+        span = x[0]
+        y, test_size, ewm_add_kwargs = args
+        with np.errstate(divide='raise', over='raise', under='raise',
+                         invalid='raise'):
+            ewm = y.ewm(span=span, **ewm_add_kwargs)
+            try:
+                loglik = np.sum(
+                    np.log(
+                        norm.pdf(
+                            x=y.tail(test_size),
+                            loc=ewm.mean().shift().tail(test_size),
+                            scale=ewm.std(ddof=1).shift().tail(test_size)
+                        )
                     )
                 )
-            ) + np.log(test_size) * span
-        )
+            except FloatingPointError:
+                loglik = -np.inf
+        return (-2 * loglik + 2 * span)
 
     def calculate_ema(self, y=None, optimize_ewm_span=False, **kwargs):
         if optimize_ewm_span or not self.ewm_span:
